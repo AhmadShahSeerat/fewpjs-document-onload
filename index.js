dd314791050db83d3fce41e2de61f30ec943b5a0
Ahmad Shah <ah.seerat@gmail.com>
2022-11-01 17:05:09 -0400

    commit tree b0ee99d01472ddd6bcaaebe2f97e56b95809d661

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-// Your code goes here
 document.addEventListener( "DOMContentLoaded", function () {
-    updateDOM()
-  } );
-  
-  
-  function updateDOM() {
-    document.getElementById( "text" )
-      .innerHTML = "This is really cool!";
-  }
+  updateDOM()
+} );
+
+
+function updateDOM() {
+  document.getElementById( "text" )
+    .innerHTML = "This is really cool!";
+}
